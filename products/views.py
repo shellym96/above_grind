888f8222e32d6440bb8179184f9aa03d60137fe7
shellym96 Maughan <shellymaughan10@gmail.com>
2024-01-16 11:22:29 +0000

    Correct spelling mistakes on views.py products

--- products/views.py
+++ products/views.py
@@ -57,11 +57,12 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show indiviual products details """
+    """ A view to show individual product details """
 
-    product = get_object_or_404(Product, pk-product_id)
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': products,
+        'product': product,
     }
-    return render(request, 'products/product_detail.html')
+
+    return render(request, 'products/product_detail.html', context)
