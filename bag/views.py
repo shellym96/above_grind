5b354da367f191994c119df5f856a4af8ea7c27b
shellym96 Maughan <shellymaughan10@gmail.com>
2024-01-16 11:21:47 +0000

    Update view to print the bag to see results

--- bag/views.py
+++ bag/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def view_bag(request):
-    """ A view that renders the contents of the bag """
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the cart """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -20,4 +20,6 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
+    print(request.session['bag'])
     return redirect(redirect_url)
+    
