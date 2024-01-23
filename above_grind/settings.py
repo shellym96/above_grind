26bd39fe969121c93c76693f388d2b880989d41c
shellym96 Maughan <shellymaughan10@gmail.com>
2024-01-23 12:28:16 +0000

    Remove secret key and set debug

--- above_grind/settings.py
+++ above_grind/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ybp&u6yzwuma0xg5^-ymjb_t0!veo*tk!=owcxk0+7mkd6us00'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-shellym96-abovegrind-5h8dzh08uc0.ws-eu107.gitpod.io', 'above-grind.herokuapp.com', 'localhost']
 
