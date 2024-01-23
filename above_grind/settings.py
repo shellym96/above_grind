b82ebebc2c2fee0f84ae28ecc892d3900bc7026c
shellym96 Maughan <shellymaughan10@gmail.com>
2024-01-23 12:07:44 +0000

    Deployment

--- above_grind/settings.py
+++ above_grind/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-ybp&u6yzwuma0xg5^-ymjb_t0!veo*tk!=owcxk0+7mkd6us00
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-shellym96-abovegrind-5h8dzh08uc0.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-shellym96-abovegrind-5h8dzh08uc0.ws-eu107.gitpod.io', 'above-grind.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +120,17 @@ WSGI_APPLICATION = 'above_grind.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
