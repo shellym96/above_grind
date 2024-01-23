4eb17abc2245a61b703a64f915a059341289f1bd
shellym96 Maughan <shellymaughan10@gmail.com>
2024-01-23 13:33:11 +0000

    Update allowed hosts for deployment

--- above_grind/settings.py
+++ above_grind/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-shellym96-abovegrind-5h8dzh08uc0.ws-eu107.gitpod.io', 'https://above-grind-d535f4f31606.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-shellym96-abovegrind-5h8dzh08uc0.ws-eu107.gitpod.io', 'https://above-grind-d535f4f31606.herokuapp.com/', 'localhost']
 
 
 # Application definition
