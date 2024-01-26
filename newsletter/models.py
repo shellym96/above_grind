b84bd81e066b070a891f3a340a36c0a4dbfcfc43
shellym96 Maughan <shellymaughan10@gmail.com>
2024-01-26 16:34:44 +0000

    Updating the newsletter models and css

--- newsletter/models.py
+++ newsletter/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Newsletter(models.Model):
     email = models.EmailField(max_length=256, null=False, blank=False)
+    
 
     def __str__(self):
         return self.email
