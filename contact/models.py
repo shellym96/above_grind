1d71f3dff4499104040599208aedd5d98a024a21
shellym96 Maughan <shellymaughan10@gmail.com>
2024-01-31 11:12:37 +0000

    Create contact model for user to contact via email

---
+++ contact/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Contact(models.Model):
+    """ Contact form model """
+    CONTACT_CHOICES = [
+        ('general', 'General Enquiry'),
+        ('order', 'Order Enquiry '),
+        ('return', 'Return Request'),
+        ('complaint', 'Complaint'),
+    ]
+    subject = models.CharField(max_length=150)
+    reason = models.CharField(max_length=45, choices=CONTACT_CHOICES)
+    message - models.TextField(max_length=800)
+    email = model.EmailField()
+
+    def __str__(self):
+        return.self.email
