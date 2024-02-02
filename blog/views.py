1c4b2918495bf30dcc08d70f7979dbdbb9f4f474
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-02 11:29:58 +0000

    Update views for blog post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def blog(request):
+
+    template = "blog/blog.html"
+    return render(request, template)
