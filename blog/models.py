41350d03c8dec9fdc11be3a0ca526692d802c97c
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-02 16:03:14 +0000

    Import product from products.models

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from products.models import Product
 
 class reviews(models.Model):
     """ Review blog for products reviews """
