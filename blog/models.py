b22373492dde797090473a0be590b565cbc96081
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-05 13:10:25 +0000

    Redone the category and comment models

--- blog/models.py
+++ blog/models.py
@@ -2,20 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
-class reviews(models.Model):
-    """ Review blog for products reviews """
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
 
-    review = models.CharField(blank=True, null=True, max_length=100)
-    title = models.CharField(max_length=130, unique=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews',)
-    updated_on = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    featured_image = models.ImageField(blank=True, upload_to='blogimages')
-    excerpt = models.TextField(blank=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ['-created_on']
+    published_date = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category)
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments',
+    null=False, blank=False)
+    name = models.CharField(max_length=80, null=False, blank=False)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    active = models.BooleanField(default=True, null=False, blank=False)
+
+    class Meta:
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
