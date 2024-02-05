e063b00fa7b22b432b62927887570b4e13383b7e
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-05 13:09:30 +0000

    Update post admin for admin panel posting blogs

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Comment, Category
+# from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    """ Posts for blogs posted """ 
+    list_display = (
+        'title',
+        'slug',
+    )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug'
+    )
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
