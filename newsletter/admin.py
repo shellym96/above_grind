3ac72d1ab7522bb9b27fdac19ac1bb9896c2163c
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-05 13:13:19 +0000

    Add newsletter admin for backend admin panel

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Newsletter
 
 # Register your models here.
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    """ Sign up to newsletter """ 
+    list_display = (
+        'email',
+        'name',
+    )
