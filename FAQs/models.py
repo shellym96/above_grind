85b692fb55565b3fe7a9db65eac439327e66a3cf
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-08 12:00:35 +0000

    Create models for FAQs add, update and delete

---
+++ FAQs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class FAQ(models.Model):
+    """ FAQs model. """
+    question = models.CharField(max_length=250, null=False, blank=False)
+    answer = models.TextField(null=False, blank=False)
+
+    class Meta:
+        verbose_name_plural = "FAQs"
+        ordering = ["question", ]
+
+    def __str__(self):
+        return self.question
