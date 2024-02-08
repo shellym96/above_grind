6d55f87573105a1d0feef9da6b180bd17282a777
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-08 12:00:46 +0000

    Add urls for html pages created for faqs

---
+++ FAQs/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.faqs, name="faqs"),
+    path("add/", views.add_faq, name="add_faq"),
+    path("update/<int:id>", views.update_faq, name="update_faq"),
+    path("delete/<int:id>", views.delete_faq, name="delete_faq"),
+    path("returns/<int:id>", views.returns, name="returns"),
+]
