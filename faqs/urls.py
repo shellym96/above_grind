895dba1a10081d38c1aacb534b1fbd494b97c7e6
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-08 16:08:27 +0000

    Change instance of FAQs to lowercase faqs, remigrate

--- faqs/urls.py
+++ faqs/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("add/", views.add_faq, name="add_faq"),
     path("update/<int:id>", views.update_faq, name="update_faq"),
     path("delete/<int:id>", views.delete_faq, name="delete_faq"),
-    path("returns/<int:id>", views.returns, name="returns"),
+    path("returns/", views.returns, name="returns"),
 ]
