1a5fdc550c92b6e31d661808ef5b8a46cf63c74f
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-14 10:02:30 +0000

    Update the fields to be required for newsletter

--- newsletter/models.py
+++ newsletter/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 class Newsletter(models.Model):
-    email = models.EmailField(max_length=256, null=True, blank=False)
-    name = models.CharField(max_length=100, null=True, blank=False)
-    
+    email = models.EmailField(max_length=256, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
         return self.email
