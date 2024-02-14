307fe2c618a097989e70137d98cd354fe6231e71
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-14 09:31:32 +0000

    Update debug as env var

--- above_grind/settings.py
+++ above_grind/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['above-grind-d535f4f31606.herokuapp.com', '8000-shellym96-abovegrind-5h8dzh08uc0.ws-eu108.gitpod.io', 'localhost']
 
