f0b942fa5a8bee16a2a0e6e9d59ad6b4f7519b22
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-20 13:18:33 +0000

    Update FAQs model fields

--- faqs/models.py
+++ faqs/models.py
@@ -5,10 +5,12 @@ class FAQ(models.Model):
     """ FAQs model. """
     question = models.CharField(max_length=250, null=False, blank=False)
     answer = models.TextField(null=False, blank=False)
+    link_text = models.CharField(max_length=100, null=True, blank=True)
+    link_url = models.CharField(max_length=100, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "FAQs"
-        ordering = ["question", ]
+        ordering = ["id", ]
 
     def __str__(self):
         return self.question
