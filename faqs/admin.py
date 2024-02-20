8aaa2b52ba5eb6072f379b088ce6dcc1703b2c0b
shellym96 Maughan <shellymaughan10@gmail.com>
2024-02-20 13:19:06 +0000

    Show all FAQs fields on admin dashboard

--- faqs/admin.py
+++ faqs/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import FAQ
 
 
-admin.site.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = (
+        'question',
+        'answer',
+        'link_text',
+        'link_url',
+    )
+
+admin.site.register(FAQ, FAQAdmin)
